0cccde780c53f6d8f18cf6df5af29c44843f1258
Ivomir1 <burian@cinovahora.cz>
2022-10-17 21:03:08 +0200

    test2

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,8 @@ use App\Http\Controllers\PostsFromUserController;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
-
+|**
+   
 */
 Route::get('/postsfromuser/{id}', [PostsFromUserController::class, 'show']);     // ukaze prispevky konkretniho uzivatele
 Route::resource('/posts', PostsController::class); //prace se vsemi prispevky
